7032448a6ed6b24b36d7fbfc94337a5a23fc4ca3
Mykola Vorotynskyy <vorotynskyy@gmail.com>
2023-06-02 10:35:07 +0200

    commit

---
+++ shop-photo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ shop-photo/src/pages/Photos.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Photos() {
+    return (
+        <main className="photos">
+            <h1>Image go here</h1>
+        </main>
+    )
+}
+
+export default Photos
